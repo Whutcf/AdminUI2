3a5defcd7e4ed84fe657e59b7d6a21bce2f83573
Whutcf <1967380642@qq.com>
2020-03-04 20:14:37 +0800

    添加readme

---
+++ src/main/java/com/smic/cf/mapper/master/UserRoleMapper.java
@@ -0,0 +1,19 @@
+package com.smic.cf.mapper.master;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.smic.cf.pojo.User;
+import com.smic.cf.pojo.UserRole;
+
+@Mapper
+public interface UserRoleMapper extends BaseMapper<UserRole> {
+
+	List<Integer> selectRoleIds(@Param(Constants.WRAPPER) Wrapper<User> wrapper);
+
+}
